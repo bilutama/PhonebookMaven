81d4c676ebb264238180d7d0252eb619f333b3ea
biluta.ma <biluta.m.a@gmail.com>
2022-02-28 16:59:07 +0100

    PhonebookMaven

--- src/main/java/ru/academits/servlet/ExportContactsServlet.java
+++ src/main/java/ru/academits/servlet/ExportContactsServlet.java
@@ -15,28 +15,21 @@ public class ExportContactsServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) {
         try (OutputStream outputStream = resp.getOutputStream()) {
+
             resp.setContentType("application/vnd.ms-excel");
             resp.setHeader("Content-Disposition", "attachment; filename=phonebook.xls");
 
-            Workbook workbook = createExcel();
-            workbook.write(outputStream);
+            Workbook workbook = new XSSFWorkbook();
+            Sheet worksheet = workbook.createSheet("TestSheet");
 
-            outputStream.flush();
+            worksheet.createRow(1).createCell(1).setCellValue("1");
+            worksheet.createRow(2).createCell(1).setCellValue("2");
+            worksheet.createRow(3).createCell(1).setCellValue("3");
+
+            workbook.write(outputStream);
         } catch (Exception e) {
             System.out.println("error in ExportContactsServlet POST: ");
             e.printStackTrace();
         }
-    }
-
-    private Workbook createExcel() {
-        Workbook workbook = new XSSFWorkbook();
-        Sheet worksheet = workbook.createSheet("Worksheet");
-
-        Row row1 = worksheet.createRow(0);
-
-        Cell cellA1 = row1.createCell(0);
-        cellA1.setCellValue("TEST DATA");
-
-        return workbook;
     }
 }
